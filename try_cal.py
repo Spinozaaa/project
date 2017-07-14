54532b2e1c9ae54346b4d5453d45f02aadf72fc6
spinozaaa <u201610378@hust.edu.cn>
2017-07-14 19:17:40 +0800

    calculate

---
+++ try_cal.py
@@ -0,0 +1,62 @@
+from tkinter import *  
+  
+class Cal(Frame):  
+    def __init__(self):  
+        Frame.__init__(self)  
+        self.pack(expand = YES,fill = BOTH)  
+        self.master.title('Calculator')  
+        self.master.geometry('400x300')  
+        self.draw()  
+          
+    def addButton(self,parent,side,text,command = None):  
+        b = Button(parent,text = text,command = command)  
+        b.pack(side = side,expand = YES,fill = BOTH) 
+        return b  
+  
+    def addFrame(self,parent,side):  
+        f = Frame(parent)  
+        f.pack(side = side,expand = YES,fill = BOTH)  
+        return f  
+  
+    def draw(self):  
+        res = StringVar()  
+        cc = StringVar()  
+        e = Entry(self,relief = SUNKEN,textvariable = cc)  
+        e.pack(side = TOP,expand = YES,fill = BOTH)  
+        r = Entry(self,relief = SUNKEN,textvariable = res)  
+        r.pack(side = TOP,expand = YES,fill = BOTH)  
+        charArray = ("123+","456-","789*","-0./","%()")  
+        for vx in charArray:  
+            keyF = self.addFrame(self,TOP)  
+            for char in vx:  
+                self.addButton(keyF,LEFT,char,  
+                               lambda o = cc,s = '%s'%char:self.showCc(o,s))  
+        self.addButton(keyF,LEFT,'=',lambda o = cc,r = res,s = self:s.cal(o,r))  
+        keyF = self.addFrame(self,TOP)  
+        self.addButton(keyF,LEFT,'Clear',  
+                       lambda o = cc,r = res:(o.set('0'),res.set('0')))  
+        self.addButton(keyF,LEFT,'Del',  
+                       lambda o = cc:o.set(o.get()[0:-1]))  
+        #self.addButton(keyF,LEFT,'Demo',None)  
+        #Str = 'input the number'  
+        #self.addButton(keyF,LEFT,'Help',lambda r = res:r.set(Str))  
+  
+    def showCc(self,cc,char):  
+        s = cc.get()  
+        if s == '0':  
+            s = ''  
+        s += char  
+        cc.set(s)  
+          
+    def cal(self,cc,r):  
+        try:  
+            r.set(eval(cc.get()))  
+        except Exception:  
+            r.set("ERROR")  
+          
+  
+def main():  
+    Cal().mainloop()  
+  
+if __name__ == '__main__':  
+    main()  
